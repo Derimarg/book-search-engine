5d8a23833b713a146c4d8d24abcbe9718f7d2cfc
Derimar Gray <derimargray@gmail.com>
2021-10-25 14:39:38 -0500

    add typeDefs query and mutatinos, add user

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,12 +1,27 @@
-// const { User, Book } = require('../models')
-// const { signToken } = require('../utils/auth')
-// const { AuthenticationError } = require('apollo-server-express');
+const { User, Book } = require("../models");
+const { signToken } = require("../utils/auth");
+const { AuthenticationError } = require("apollo-server-express");
+
 const resolvers = {
-    Query: {
-      helloWorld: () => {
-        return 'Hello world!';
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.finOne({ _id: context.user._id }).select(
+          "-__v -password"
+        );
+        return userData;
       }
-    }
-  };
-  
-  module.exports = resolvers;
+      throw new AuthenticationError("Not logged in!");
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+  },
+};
+
+module.exports = resolvers;
